240a9f50e36ff6ea38e227526f0f57899920100e
DragonfireHD98 <156013157+DragonfireHD98@users.noreply.github.com>
2024-02-02 00:24:02 +0300

    Update Main.php

--- src/SchdowNVIDIA/OreEarn/Main.php
+++ src/SchdowNVIDIA/OreEarn/Main.php
@@ -47,7 +47,7 @@ class Main extends PluginBase
         }
     }
 
-    public final function cfgChecker() {
+        protected function cfgChecker() {
         $cfgVersion = 4;
         $msgVersion = 1;
         if(($this->getConfig()->get("cfg-version")) < $cfgVersion || !($this->getConfig()->exists("cfg-version"))) {
