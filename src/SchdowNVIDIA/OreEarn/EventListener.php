269800f77b73229f27a46e94d5f3e0523b50c2f7
DragonfireHD98 <156013157+DragonfireHD98@users.noreply.github.com>
2024-02-01 23:53:10 +0300

    Update EventListener.php

--- src/SchdowNVIDIA/OreEarn/EventListener.php
+++ src/SchdowNVIDIA/OreEarn/EventListener.php
@@ -27,7 +27,7 @@ declare(strict_types = 1);
 
 namespace SchdowNVIDIA\OreEarn;
 
-use onebone\economyapi\EconomyAPI;
+use cooldogedev\BedrockEconomy\api\legacy\ClosureContext;
 use pocketmine\block\Block;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
